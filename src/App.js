7b0dad8095f448279a121acb230cdb8918b8b64b
souvighosh <souvik.personal.email@gmail.com>
2023-06-22 01:04:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="header-image" />
+        <div className="header-intro">
+          <h1>Souvik Ghosh</h1>
+          <h4>Full-Stack Developer | Front-End Specialist</h4>
+          <p>
+            Innovative and experienced software professional with a proven track
+            record of success in full-stack development and a specialization in
+            front-end technologies. With a solid foundation in Java full-stack
+            development, I transitioned into consulting and application support
+            for several years before returning to my passion for product
+            development. I am currently thriving as a Full Stack Developer,
+            leveraging my strong skill set in front-end development with
+            JavaScript, React, HTML, and CSS, along with expertise in backend
+            API development using Ruby on Rails. With over 11 years of
+            comprehensive industry experience, I bring a unique blend of
+            technical proficiency, strategic thinking, and client-oriented
+            approach to drive impactful solutions.
+          </p>
+        </div>
+      </div>
+      <div className="contact">
+        <div className="contact-col">
+          <div>souvik.work.email@gmail.com</div>
+          <div>Hyderabad, India</div>
+          <div>https://www.linkedin.com/in/souvik-ghosh-57136663/</div>
+        </div>
+        <div className="contact-col">
+          <div>souvik.work.email@gmail.com</div>
+          <div>Hyderabad, India</div>
+          <div>https://www.linkedin.com/in/souvik-ghosh-57136663/</div>
+        </div>
+      </div>
     </div>
   );
 }
